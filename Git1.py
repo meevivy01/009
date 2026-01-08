c872e813d04737e43884ff37dded6d0c40e351e4
meevivy01 <meevivy@gmail.com>
2026-01-08 23:19:47 +0700

    Git1.py

--- Git1.py
+++ Git1.py
@@ -95,7 +95,7 @@ if os.path.exists(CLIENTS_PATH):
                 elif not isinstance(CLIENTS_TARGETS[k], list): CLIENTS_TARGETS[k] = [str(CLIENTS_TARGETS[k])]
     except: pass
 
-TARGET_UNIVERSITIES = ["นเรศวร", "มหาลัยนเรศวร , "Naresuan University"]  
+TARGET_UNIVERSITIES = ["นเรศวร", "มหาลัยนเรศวร" , "Naresuan University"]  
 TARGET_FACULTIES = ["เครื่องสำอาง","Cosmetic Science"] 
 TARGET_MAJORS = ["เครื่องสำอาง", "วิทยาศาสตร์เครื่องสำอาง","Cosmetic Science", "Cosmetics", "Cosmetic"]
 SEARCH_KEYWORDS = ["นเรศวร เครื่องสำอาง"]
